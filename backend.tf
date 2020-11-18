caa1ca18bb69e90a5bd2ba9feb6490deea7d6fca
Islam Kambarov <ikambarov@yahoo.com>
2020-11-18 21:20:36 +0000

    Use Kubernetes backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,4 @@
-backend "s3" {
-  bucket  = "ec-k8-terraform"
-  key     = "pod/terraform.tfstate"
-  region  = "us-east-1"
-  encrypt = "true"
-}
+backend "kubernetes" {
+    secret_suffix    = "state"
+  }
+
